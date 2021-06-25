e1460ab8073274872d2215881731661eb4e497c4
Ricardo Freitas <ricardo.freitas@acct.global>
2021-06-25 15:18:04 -0300

    Bearer Auth Middleware, and ensureAdmin Refactoring

---
+++ src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,24 @@
+import { NextFunction, Request, Response } from 'express'
+import { verify } from 'jsonwebtoken'
+
+interface IPayload {
+  sub: string
+}
+
+export function ensureAuthenticated (request: Request, response: Response, next: NextFunction) {
+  const authToken = request.headers.authorization
+
+  if (!authToken) return response.status(401).end()
+
+  const [, token] = authToken.split(' ')
+
+  try {
+    const { sub } = verify(token, '18abb4747aefe043d916e7455db35f93') as IPayload
+
+    request.user_id = sub
+
+    return next()
+  } catch (error) {
+    return response.status(401).end()
+  }
+}
