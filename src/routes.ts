736be4c1a9c576402befb93f7cec60d7ca7ff45c
Ricardo Freitas <ricardo.freitas@acct.global>
2021-06-25 15:37:19 -0300

    Get sent and received Compliments

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,8 @@ import { AuthenticateUserController } from './controllers/AuthenticateUserContro
 import { CreateComplimentController } from './controllers/CreateComplimentController'
 import { CreateTagController } from './controllers/CreateTagController'
 import { CreateUserController } from './controllers/CreateUserController'
+import { ListUserReceivedComplimentsController } from './controllers/ListUserReceivedComplimentsController'
+import { ListUserSentComplimentsController } from './controllers/ListUserSentComplimentsController'
 import { ensureAdmin } from './middlewares/ensureAdmin'
 import { ensureAuthenticated } from './middlewares/ensureAuthenticated'
 
@@ -11,10 +13,15 @@ const createUserController = new CreateUserController()
 const createTagController = new CreateTagController()
 const authenticateUserController = new AuthenticateUserController()
 const createComplimentController = new CreateComplimentController()
+const listUserSentComplimentsController = new ListUserSentComplimentsController()
+const listUserReceivedComplimentsController = new ListUserReceivedComplimentsController()
 
 router.post('/users', createUserController.handle)
 router.post('/tags', ensureAuthenticated, ensureAdmin, createTagController.handle)
 router.post('/login', authenticateUserController.handle)
 router.post('/compliments', ensureAuthenticated, createComplimentController.handle)
+
+router.get('/users/sent', ensureAuthenticated, listUserSentComplimentsController.handle)
+router.get('/users/received', ensureAuthenticated, listUserReceivedComplimentsController.handle)
 
 export { router }
